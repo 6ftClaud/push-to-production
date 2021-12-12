96312e9b17b26bec3f55cdd544324a0c4daf2c12
Matas Ladyga <matthew.ladyg@gmail.com>
2021-12-12 18:40:45 +0200

    auth fix

--- misc/instantiate.sh
+++ misc/instantiate.sh
@@ -9,7 +9,7 @@ CVMID=$(echo $CVMREZ | cut -d ' ' -f 3)
 echo $CVMID
 echo "Waiting for VM to RUN 30 sec."
 sleep 30
-$(onevm show $CVMID --user $CUSER --password $CPASS --endpoint $CENDPOINT > ${CVMID}.txt)
+$(onevm show $CVMID > ${CVMID}.txt)
 CSSH_CON=$(cat $CVMID.txt | grep CONNECT\_INFO1| cut -d '=' -f 2 | tr -d '"')
 CSSH_PRIP=$(cat $CVMID.txt | grep PRIVATE\_IP| cut -d '=' -f 2 | tr -d '"')
 echo "Connection string: $CSSH_CON"
